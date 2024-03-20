a89893cbe645e85da12399b907bfa0c763f8b180
Jeffrey191997 <56708515+Jeff191997@users.noreply.github.com>
2024-03-21 03:22:01 +0700

    [Added Spin Animation to Loading Screen]

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:clima/screens/location_screen.dart';
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = 'f90d2213c98e1bccdd668ca7859f0343';
 
@@ -45,7 +46,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
 
